f885c9115fa85315356f854a4f70516c002af309
siotan <nyaa.-525@outlook.jp>
2023-02-05 06:19:01 +0900

    add animation

--- src/components/AnimationCanvas.jsx
+++ src/components/AnimationCanvas.jsx
@@ -1,11 +1,13 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
 import { Points } from "./Points";
 
 export const AnimationCanvas = () => {
   return (
     <Canvas camera={{ position: [100, 10, 0], fov: 75 }}>
+      <OrbitControls />
       <Suspense fallback={null}>
         <Points />
       </Suspense>
--- src/components/Points.jsx
+++ src/components/Points.jsx
@@ -1,11 +1,38 @@
-import { useMemo } from "react";
-import { useLoader } from "@react-three/fiber";
-import * as THREE from "three";
+import { useCallback, useMemo, useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { useTexture } from "@react-three/drei";
 
 import circleImg from "../assets/circle.png";
 
 export const Points = () => {
-  const img = useLoader(THREE.TextureLoader, circleImg);
+  const img = useTexture(circleImg);
+  const bufferRef = useRef();
+
+  useFrame(() => {
+    t += 15;
+    const positions = bufferRef.current.array;
+
+    let i = 0;
+    for (let xi = 0; xi < count; xi++) {
+      for (let zi = 0; zi < count; zi++) {
+        let x = sep * (xi - count / 2);
+        let z = sep * (zi - count / 2);
+        positions[i + 1] = graph(x, z);
+        i += 3;
+      }
+    }
+    bufferRef.current.needsUpdate = true;
+  });
+
+  let t = 0;
+  let f = 0.002;
+  let a = 3;
+  const graph = useCallback(
+    (x, z) => {
+      return Math.sin(f * (x ** 2 + z ** 2 + t)) * a;
+    },
+    [t, f, a]
+  );
 
   const count = 100;
   const sep = 3;
@@ -15,19 +42,20 @@ export const Points = () => {
       for (let zi = 0; zi < count; zi++) {
         let x = sep * (xi - count / 2);
         let z = sep * (zi - count / 2);
-        let y = 0;
+        let y = graph(x, z);
         positions.push(x, y, z);
       }
     }
 
     return new Float32Array(positions);
-  }, [count, sep]);
+  }, [count, sep, graph]);
 
   return (
     <points>
       <bufferGeometry attach="geometry">
         <bufferAttribute
           attach="attributes-position"
+          ref={bufferRef}
           array={planePositions}
           count={planePositions.length / 3}
           itemSize={3}
